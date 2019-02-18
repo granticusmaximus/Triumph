be681a58c9cf6bc1954f0e3c07938b0adb96791b
Grant Watson <lordcappello@outlook.com>
2019-02-18 17:21:36 -0500

    Update

---
+++ WebApp/Controllers/BaseApiController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class BaseApiController : ApiController
+    {
+        //Code removed from brevity
+        private ApplicationRoleManager _AppRoleManager = null;
+
+        protected ApplicationRoleManager AppRoleManager
+        {
+            get
+            {
+                return _AppRoleManager ?? Request.GetOwinContext().GetUserManager<ApplicationRoleManager>();
+            }
+        }
+    }
+}
--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Owin;
 using Owin;
+using WebApp.Models;
 
 [assembly: OwinStartupAttribute(typeof(WebApp.Startup))]
 namespace WebApp
@@ -9,6 +10,17 @@ namespace WebApp
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+        }
+
+        private void ConfigureOAuthTokenGeneration(IAppBuilder app)
+        {
+            // Configure the db context and user manager to use a single instance per request
+            //Rest of code is removed for brevity
+
+            app.CreatePerOwinContext<ApplicationRoleManager>(ApplicationRoleManager.Create);
+
+            //Rest of code is removed for brevity
+
         }
     }
 }
